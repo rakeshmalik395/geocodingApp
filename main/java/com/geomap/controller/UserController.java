b4677954e367cab8708de1754456d9c29fd49cdc
rakeshmalik395 <lilgolu395@gmail.com>
2019-12-27 17:33:32 +0530

    Revert "initial commit"
    This reverts commit 40cf5e9c3ef40a1534bb186f664c2d843c83e4dd.

--- main/java/com/geomap/controller/UserController.java
+++
@@ -1,5 +0,0 @@
-package com.geomap.controller;
-
-public class UserController {
-
-}
--- main/java/com/geomap/service/UserMarkerService.java
+++
@@ -1,9 +0,0 @@
-package com.geomap.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserMarkerService {
-
-	
-}
